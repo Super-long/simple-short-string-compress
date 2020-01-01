d3b159fc5e5963b833be39147b460b2ec296c482
Super-long <46051144+Super-long@users.noreply.github.com>
2020-01-01 14:16:44 +0800

    Update smaz.cc

--- smaz.cc
+++ smaz.cc
@@ -3,11 +3,11 @@
 //
 
 #include <iostream>
-#include "../base/decondig.h"
-#include "../base/decondig.cc"
+#include "decondig.h"
+#include "decondig.cc"
 int main(){
     std::string c = "Nothing is more difficult, and therefore more precious, than to be able to decide";
-   　std::string d;
+    std::string d;
     deconding::smaz_compress(c.data(),c.size(),&d);
     std::cout << d<< std::endl;
     std::string p ;
